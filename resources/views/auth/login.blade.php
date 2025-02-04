93fd38c71536c7aca9158e69183ddfff59a4e0cc
Dodge Nicholson <dodgesuico.dev@gmail.com>
2025-02-04 11:14:42 +0800

    Generating Pages

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.default")
+@section("Title", "Login")
+@section("content")
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view("/", "welcome");
+
+Route::get("/login", [AuthController::class, "login"]);
