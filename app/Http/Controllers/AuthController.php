5a52d242b46a6770682462c53a944877800cebf3
Dodge Nicholson <dodgesuico.dev@gmail.com>
2025-02-04 11:19:43 +0800

    Again

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view("auth.login");
     }
 }
