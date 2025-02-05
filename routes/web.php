3bd5002207fcd99bc8c021167ed6549e687ebc56
Dodge Nicholson <dodgesuico.dev@gmail.com>
2025-02-05 14:00:32 +0800

    yes

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::view("/", "index")->name('index');
-
-Route::get("/teacher_dashboard", [TeacherController::class, "teacher"])->name('teacher_dashboard');
-
-Route::middleware("auth")->group(function () {
-    Route::view("/welcome", "welcome")->name("welcome");
+Route::middleware("auth")->group(function (){
+    Route::view("/", "welcome")->name("welcome");
 });
+
+Route::view("/", "welcome")->name("welcome");
 
 Route::get("/login", [AuthController::class, "login"])->name('login');
 Route::get("/register", [AuthController::class, 'register'])->name('register');
+
+
 Route::post("/login", [AuthController::class, 'LoginPost'])->name('login.post');
 Route::post("/register", [AuthController::class, 'RegisterPost'])->name('register.post');
-Route::post('/logout', function () {
-    Auth::logout();
-    return redirect('/login')->with('success', 'You have been logged out successfully.');
-})->name('logout');
+Route::post('/logout', function () {Auth::logout(); return redirect('/login')->with('success', 'You have been logged out successfully.'); })->name('logout');
