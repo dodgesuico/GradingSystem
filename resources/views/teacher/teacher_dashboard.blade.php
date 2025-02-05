90d20b367153670389f84cdd9fe989a9ca2775ec
Khemark Ocariza <khemark.ocariza@gmail.com>
2025-02-05 13:16:25 +0800

    teacher ui added

---
+++ resources/views/teacher/teacher_dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends("layouts.teacher")
+<title>@yield("title", "CKCM Grading | Teacher Home")</title>
+@section("content")
+
+
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TeacherController;
 
-Route::view("/", "welcome");
+Route::view("/", "index")->name('index');
+
+
+Route::get("/teacher_dashboard", [TeacherController::class, "teacher"])->name('teacher_dashboard');
 
 Route::get("/login", [AuthController::class, "login"])->name('login');
 Route::get("/register", [AuthController::class, 'register'])->name('register');
