5b717bad756363bb8268f57d1d28d3554ff31c2e
Dodge Nicholson <dodgesuico.dev@gmail.com>
2025-02-05 15:47:56 +0800

    5252

--- resources/views/teacher/teacher_dashboard.blade.php
+++ resources/views/teacher/teacher_dashboard.blade.php
@@ -2,6 +2,13 @@
 <title>@yield("title", "CKCM Grading | Teacher Home")</title>
 @section("content")
 
+<div>
+    @if (Auth::check())
+        <p>Welcome, {{ Auth::user()->name }}!</p>
+        <p>Your Role is, {{ Auth::user()->role }}!</p>
+    @endif
+</div>
+
 
 @endsection
 
