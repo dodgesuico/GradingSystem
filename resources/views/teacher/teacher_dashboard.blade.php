6d320f7a71a037c6943501a8b33f0b49c86d0504
Khemark Ocariza <khemark.ocariza@gmail.com>
2025-02-06 10:15:23 +0800

    teacher dashboard fixed

--- resources/views/teacher/teacher_dashboard.blade.php
+++ resources/views/teacher/teacher_dashboard.blade.php
@@ -2,13 +2,31 @@
 <title>@yield("title", "CKCM Grading | Teacher Home")</title>
 @section("content")
 
-<div>
+<div class="dashboard">
     @if (Auth::check())
-        <p>Welcome, {{ Auth::user()->name }}!</p>
-        <p>Your Role is, {{ Auth::user()->role }}!</p>
+    <h1>Welcome, {{ Auth::user()->name }}!</h1>
+    <h2>Your Role is, {{ Auth::user()->role }}!</h2>
     @endif
 </div>
 
 
 @endsection
 
+
+<style>
+    .dashboard{
+        margin-top: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .dashboard h1{
+        color: var(--ckcm-color4)
+    }
+    .dashboard h2{
+        color: var(--color6);
+    }
+</style>
