c1fc44d33637c7a1756973b95f55d3e494b309e6
Dodge Nicholson <dodgesuico.dev@gmail.com>
2025-03-19 16:01:48 +0800

    again

--- resources/views/registrar/registrar_classes_view.blade.php
+++ resources/views/registrar/registrar_classes_view.blade.php
@@ -856,6 +856,21 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <h2 style="margin: 10px 0">Grades</h2>
 
         @if ($finalGrades->where('status', 'Locked')->isEmpty() && $classes_student->isNotEmpty())
